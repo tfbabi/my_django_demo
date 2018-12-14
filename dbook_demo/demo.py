5fa17c6c4024fdbc0ce3a319d3eba478b7406414
yannis <tfbabi@yeah.net>
2018-12-14 23:14:08 +0800

    Delete demo.py

--- dbook_demo/demo.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2018/12/2 0:14
-# @Author  : yannis
-# @Email   : tfbabi@163.com
-# @File    : demo.py
-# @Software: PyCharm
-
-a=['抽烟','喝酒','打飞机']
-print ','.join(a)
