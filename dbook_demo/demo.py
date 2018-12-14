9275d7708f111afcec2adefa6cfe0a832c4806ef
yannis <tfbabi@yeah.net>
2018-12-14 23:05:09 +0800

    Add files via upload

---
+++ dbook_demo/demo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/2 0:14
+# @Author  : yannis
+# @Email   : tfbabi@163.com
+# @File    : demo.py
+# @Software: PyCharm
+
+a=['抽烟','喝酒','打飞机']
+print ','.join(a)
